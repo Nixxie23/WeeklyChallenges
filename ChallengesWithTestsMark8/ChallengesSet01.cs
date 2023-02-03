7144dd90c604ae7f96ebaba1208fad073e5a786c
taylor Schuchter <taylorschuchter@gmail.com>
2023-02-03 14:21:52 -0500

    week one challenge

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -1,49 +1,70 @@
 ﻿using System;
 
-namespace ChallengesWithTestsMark8
+namespace ChallengesWithTestsMark8;
+
+public class ChallengesSet01
 {
-    public class ChallengesSet01
+    public bool AreTwoNumbersTheSame(int num1, int num2)
     {
-        public bool AreTwoNumbersTheSame(int num1, int num2)
+        if(num1 == num2)
         {
-            if(num1 == num2)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public double Subtract(double minuend, double subtrahend)
+    {
+        double answer = minuend- subtrahend;
+        return answer;
+    }
+
+    public int Add(int number1, int number2)
+    {
+        int answer = number1 + number2;
+        return answer;
+    }
+
+    public int GetSmallestNumber(int number1, int number2)
+    {
+        if (number1 < number2)
+        {
+            return number1; 
+                }
+        else if (number1 > number2)
+        {
+            return number2;
+        }
+        else
+        {
+            return number1; 
+        }
+    }
+
+    public long Multiply(long factor1, long factor2)
+    {
+        long answer = factor1 * factor2;     
+        return answer;
+    }
+
+    public string GetGreeting(string nameOfPerson)
+    {
+        if (nameOfPerson == "")
+        {
+            return "Hello!";
         }
 
-        public double Subtract(double minuend, double subtrahend)
+        else
         {
-            throw new NotImplementedException();
+            return $"Hello, {nameOfPerson}!";
         }
+    }
 
-        public int Add(int number1, int number2)
-        {
-            throw new NotImplementedException();
-        }
-
-        public int GetSmallestNumber(int number1, int number2)
-        {
-            throw new NotImplementedException();
-        }
-
-        public long Multiply(long factor1, long factor2)
-        {
-            throw new NotImplementedException();
-        }
-
-        public string GetGreeting(string nameOfPerson)
-        {
-            throw new NotImplementedException();
-        }
-
-        public string GetHey()
-        {
-            throw new NotImplementedException();
-        }
+    public string GetHey()
+    {
+     return "HEY!";
     }
 }
